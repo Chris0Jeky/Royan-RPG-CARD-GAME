2182869d4a1a87ddfcb1943a9eccdddd9e72f31a
Chris0Jeky <jeky.tck@gmail.com>
2024-06-21 00:22:21 +0100

    Create Player.java

---
+++ src/com/chris/cardgame/Player.java
@@ -0,0 +1,11 @@
+package com.chris.cardgame;
+
+public class Player {
+    private String playerID;
+    private String playerName;
+    private int health;
+
+    public Player(String playerID, String playerName, int health) {
+
+    }
+}
