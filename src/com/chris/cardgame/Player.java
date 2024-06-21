a6e1bde5a62401830819ec9a350b6b8d9404e827
Chris0Jeky <jeky.tck@gmail.com>
2024-06-21 03:08:46 +0100

    added getters for player

--- src/com/chris/cardgame/Player.java
+++ src/com/chris/cardgame/Player.java
@@ -6,6 +6,23 @@ public class Player {
     private int health;
 
     public Player(String playerID, String playerName, int health) {
+        this.playerID = playerID;
+        this.playerName = playerName;
+        this.health = health;
+    }
 
+    public String getPlayerID() {
+        System.out.println("Player id: " + playerID);
+        return playerID;
+    }
+
+    public String getPlayerName() {
+        System.out.println("Player name: " + playerName);
+        return playerName;
+    }
+
+    public int getHealth() {
+        System.out.println("Player health: " + health);
+        return health;
     }
 }
