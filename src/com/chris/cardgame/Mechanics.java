b57fd3d2b09c3d65e669adfe4347fd6a894cfc38
Chris0Jeky <jeky.tck@gmail.com>
2024-06-21 03:08:30 +0100

    bogus calls for testing

--- src/com/chris/cardgame/Mechanics.java
+++ src/com/chris/cardgame/Mechanics.java
@@ -3,5 +3,12 @@ package com.chris.cardgame;
 public class Mechanics {
     public static void start_game(){
         System.out.println("Game starting...");
+        Card card1 = new Card("Card1", "Attack", 5);
+        Player player1 = new Player("0012f", "Player1", 100);
+
+        card1.displayCardFields();
+        card1.getCardName();
+        player1.getPlayerID();
+        player1.getPlayerName();
     }
 }
