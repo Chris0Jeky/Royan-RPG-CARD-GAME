ecbacd97ef1d91eb2431a1d728c327b75ef4b7c3
Chris0Jeky <jeky.tck@gmail.com>
2024-06-23 14:06:47 +0100

    minor changes and adjustments for setters and initialisation

--- src/com/chris/cardgame/Card.java
+++ src/com/chris/cardgame/Card.java
@@ -4,23 +4,29 @@ public class Card {
     enum AdvantageType {
         A, B, C
     }
+    enum Rarity {
+        A, B, C, D
+    }
 
     private final String cardName;
     private final String type;
     private int attackVal;
     private int defenceVal;
-    private AdvantageType advantageType;
-    private int effect;
-    private boolean oneTime;
+    private final AdvantageType advantageType;
+    private int effect = 0;
+    private boolean oneTime = false;
+    private int level = 1;
+    private final Rarity rarity;
 
-    public Card(String cardName, String type, int attackVal, int defenceVal, AdvantageType advantageType) {
+
+    public Card(String cardName, String type, int attackVal, int defenceVal, AdvantageType advantageType, int level, Rarity rarity) {
         this.cardName = cardName;
         this.type = type;
         this.attackVal = attackVal;
         this.defenceVal = defenceVal;
         this.advantageType = advantageType;
-        this.effect = 0;
-        this.oneTime = false;
+        this.level = level;
+        this.rarity = rarity;
 
     }
 
@@ -50,9 +56,17 @@ public class Card {
         this.attackVal = attackVal;
     }
 
+    public void addToAttackVal(int num) {
+        this.attackVal = this.attackVal + num;
+    }
+
     public int getDefenceVal() {
         System.out.println("Defence value: " + defenceVal);
         return defenceVal;
+    }
+
+    public void addToDefenceVal(int num) {
+        this.defenceVal = this.defenceVal + num;
     }
 
     public void setDefenceVal(int defenceVal) {
@@ -64,7 +78,5 @@ public class Card {
         return advantageType;
     }
 
-    public void addToDefenceVal(int defenceVal) {
-        this.defenceVal = defenceVal;
-    }
+
 }
