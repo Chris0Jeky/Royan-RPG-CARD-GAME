0b4cbcb1022a510207ce7efbc381aa4b50d4c1b4
Chris0Jeky <jeky.tck@gmail.com>
2024-06-24 02:41:55 +0100

    Update Mechanics.java

--- src/com/chris/cardgame/Mechanics.java
+++ src/com/chris/cardgame/Mechanics.java
@@ -3,10 +3,5 @@ package com.chris.cardgame;
 public class Mechanics {
     public static void start_game(){
         System.out.println("Game starting...");
-
-        card1.displayCardFields();
-        card1.getCardName();
-        player1.getPlayerID();
-        player1.getPlayerName();
     }
 }
